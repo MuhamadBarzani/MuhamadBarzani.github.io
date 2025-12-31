a21d3d9d44a33fc9ec635aa1031ff7134517568a
Muhamad Musa <barzandolamari3@gmail.com>
2026-01-01 02:05:32 +0300

    refactor: enhance ListGroup component with item selection functionality

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
+
 function ListGroup() {
   const items: string[] = ["Muhamad", "Bahroz", "Zheer", "Sami"];
-
+  function handleClick(index: number) {
+    console.log(index + "-" + items[index]);
+    setSelectedIndex(index);
+  }
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   function getList() {
     if (items.length === 0) {
       return <p>No Items Returned</p>;
     } else {
       return (
         <ul className="list-group">
-          {items.map((item) => (
-            <li className="list-group-item">{item}</li>
+          {items.map((item, index) => (
+            <li
+              className={
+                selectedIndex === index
+                  ? "list-group-item active"
+                  : "list-group-item"
+              }
+              key={item}
+              onClick={() => handleClick(index)}
+            >
+              {item}
+            </li>
           ))}
         </ul>
       );
